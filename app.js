065540706a1ab05c5ecc96b96539d5510b37f94b
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2011-04-08 21:09:01 +0100

    wrapped an if statement to check if current site is github

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-if(window.location.host !== 'github') {
-  return false;
-}
+if(window.location.host === 'github.com') {
+  function showSearch() {
 
-
-function showSearch() {
-
-}
-
-showSearch();
-$('.news div.alert').each(function() {
-  var currentElement = $(this);
-  if(currentElement.find('a[href=/joliss]').length === 0) {
-    currentElement.css('background-color', 'lightblue');
   }
-});
+
+  showSearch();
+  $('.news div.alert').each(function() {
+    var currentElement = $(this);
+    if(currentElement.find('a[href=/joliss]').length === 0) {
+      currentElement.css('background-color', 'lightblue');
+    }
+  });
+}
+else {
+  alert("You're not on Github!");
+}
