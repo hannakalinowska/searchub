6f86f81dd897daefac1a7b7e16dbdfb32136c7d7
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2011-04-08 21:24:54 +0100

    filter by username

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 if(window.location.host === 'github.com') {
-  function showSearch() {
+  $('#searchub_filter').live('submit', function() {
+    filter($('#searchub_filter input[type=text]').attr('value'));
+    return false;
+  });
 
+  function showSearch() {
+    $('.pagehead').append('<div><form id="searchub_filter">Filter: <input type="text" /><input type="submit" name="ok"/></form></div>');
+  }
+
+  function filter(query) {
+    $('.news div.alert').each(function() {
+      var currentElement = $(this);
+      if(currentElement.find('a[href=/' + query + ']').length === 0) {
+        currentElement.hide();
+      }
+      else {
+        currentElement.show();
+      }
+    });
   }
 
   showSearch();
-  $('.news div.alert').each(function() {
-    var currentElement = $(this);
-    if(currentElement.find('a[href=/joliss]').length === 0) {
-      currentElement.css('background-color', 'lightblue');
-    }
-  });
 }
 else {
   alert("You're not on Github!");
