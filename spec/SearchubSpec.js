769689871bd45494bb13df41017719e1321f99be
Hanna Kalinowska <hanna.kalinowska@gmail.com>
2011-04-20 22:19:42 +0100

    query building mechanism; one failing spec - cant compare arrays in a simple way

--- spec/SearchubSpec.js
+++ spec/SearchubSpec.js
@@ -26,4 +26,56 @@ describe('Searchub', function() {
       expect($('#searchub_filter')).toExist();
     });
   });
+
+  describe('#filter', function() {
+    it('filters by username or repo', function() {
+      spyOn(Searchub, 'buildQuery');
+
+      Searchub.filter('rickastley');
+
+      expect(Searchub.buildQuery).toHaveBeenCalled();
+    });
+  });
+
+  describe('#buildQuery', function() {
+    it('returns an object with lists of user/repo names', function() {
+      spyOn(Searchub, 'extractUserNames');
+      spyOn(Searchub, 'extractRepoNames');
+      spyOn(Searchub, 'buildUserQuery');
+      spyOn(Searchub, 'buildRepoQuery');
+
+      Searchub.buildQuery('repo:node user:astley');
+
+      expect(Searchub.extractUserNames).toHaveBeenCalled();
+      expect(Searchub.extractRepoNames).toHaveBeenCalled();
+      expect(Searchub.buildUserQuery).toHaveBeenCalled();
+      expect(Searchub.buildRepoQuery).toHaveBeenCalled();
+    });
+  });
+
+  describe('#extractUserNames', function() {
+    it('returns an array of specified user names', function() {
+      expect(Searchub.extractUserNames('rick repo:node user:astley')).toEqual(['rick', 'astley']);
+    });
+  });
+
+  describe('#extractRepoNames', function() {
+    it('returns an array of specified repo names', function() {
+      expect(Searchub.extractRepoNames('rick repo:node user:astley')).toEqual(['rick', 'node']);
+    });
+  });
+
+  describe('#buildUserQuery', function() {
+    it('builds a user CSS query from an array of user names', function() {
+      expect(Searchub.buildUserQuery(['rick', 'astley'])).
+        toEqual('a[href=/rick], a[href=/astley]');
+    });
+  });
+
+  describe('#buildRepoQuery', function() {
+    it('builds a repo CSS query from an array of repo names', function() {
+      expect(Searchub.buildRepoQuery(['rick', 'node'])).
+        toEqual('a[href$=/rick]', 'a[href$=/node]');
+    });
+  });
 });
--- src/searchub.js
+++ src/searchub.js
@@ -4,19 +4,71 @@ var Searchub = {
     this.showSearch();
   },
 
-  filter: function(query) {
-    var userQuery = 'href=/' + query;
-    var repoQuery = 'href$=/' + query;
+  filter: function(formInput) {
+    var cssQuery = this.buildQuery(formInput);
 
     $('.news div.alert').each(function() {
       var currentElement = $(this);
-      if(currentElement.find('a[' + repoQuery + ']').length === 0) {
-        currentElement.hide();
-      }
-      else {
+      if(currentElement.find(cssQuery).length) {
         currentElement.show();
       }
+      else {
+        currentElement.hide();
+      }
     });
+  },
+
+  buildQuery: function(query) {
+    var userNames = this.extractUserNames(query);
+    var repoNames = this.extractRepoNames(query);
+
+    var userCssQuery = this.buildUserQuery(userNames);
+    var repoCssQuery = this.buildRepoQuery(repoNames);
+
+    var cssQuery = [userCssQuery, repoCssQuery].join(', ');
+    return cssQuery;
+  },
+
+  extractUserNames: function(query) {
+    var userNames = [];
+    var elements = query.split(' ');
+
+    for(var i in elements) {
+      if(name = elements[i].match(/^(?:user:)?(\w+)$/)) {
+        userNames.push(name[1]);
+      }
+    }
+
+    return userNames;
+  },
+
+  extractRepoNames: function(query) {
+    var repoNames = [];
+    var elements = query.split(' ');
+
+    for(var i in elements) {
+      if(name = elements[i].match(/^(?:repo:)?(\w+)$/)) {
+        repoNames.push(name[1]);
+      }
+    }
+
+    return repoNames;
+  },
+
+  buildUserQuery: function(userNames) {
+    var userQuery = [];
+    for(var i in userNames) {
+      userQuery.push('a[href=/' + userNames[i] + ']');
+    }
+    return userQuery.join(', ');
+  },
+
+  buildRepoQuery: function(repoNames) {
+    var repoQuery = [];
+    for(var i in repoNames) {
+      repoQuery.push('a[href$=/' + repoNames[i] + ']');
+    }
+    return repoQuery.join(', ');
   },
 
   unfilter: function() {
